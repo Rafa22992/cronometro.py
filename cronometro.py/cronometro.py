717c01e904a3fca1bd65aaa9e4483f087e932c96
Rafael Pinheiro <rafael_dev22992@hotmail.com>
2023-04-29 16:14:37 -0300

    alteração formatação

--- cronometro.py/cronometro.py
+++ cronometro.py/cronometro.py
@@ -1,20 +1,34 @@
 from time import  sleep
 
+bordas = '-' * 15
+
 def cronometro():
-    c = int( input('Digite o tempo no cronômetro:'))
+    c = int( input('Digite o tempo no cronômetro: '))
+    print(bordas)
     while c > 0:
      c-= 1
      yield c
      sleep(1)
      if c == 6:
+         print('=' * 15)
          print('esta acabando seu tempo')
+         print('=' * 15)
      if c == 0:
+         print('=' * 15)
          print('Acabou o tempo')
+         print('=' * 15)
 
 t = cronometro()
 
 for tempo in t:
    if tempo % 60 == 0:
-       print(f'{tempo}min')
+       formata_min = f'{tempo}min'
+       min = f'{formata_min:_^15}'
+       print(f'{min}')
+       print(bordas)
+
    else:
-       print(f'{tempo}s')
+       formata_seg = f'{tempo}s'
+       seg = f'{formata_seg:-^15}'
+       print(f'{seg}')
+       print(bordas)
